b0fec47c3aefdf63dae8d755f6eb3d73c0952576
bilol.kobilov <bilol.kobilov.0206@users.noreply.github.com>
2023-01-08 14:46:00 +0500

    completed

---
+++ task 1/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace homework4_classes
+{
+    class Person
+    {
+        private string name;
+        private string name1;
+        private string name2;
+
+        public Person(string aname, string aname1, string aname2)
+        {
+            name = aname;
+            name1 = aname1;
+            name2 = aname2;
+        }
+
+        public string aname
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string aname1
+        {
+            get
+            {
+                return name1;
+            }
+            set
+            {
+                name1 = value;
+            }
+        }
+        public string aname2
+        {
+            get
+            {
+                return name2;
+            }
+            set
+            {
+                name2 = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Hello! My Name is "+  name + "\n"+ "Hello! My Name is " + name1 + "\n"+ "Hello! My Name is " + name2;
+        }
+    }    
+}
---
+++ task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static homework4_classes.Person;
+
+namespace homework4_classes
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] Person1 = new Person[1];
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.Write("Name: ");
+                string name = Convert.ToString(Console.ReadLine());
+                Console.Write("Name: ");
+                string name1 = Convert.ToString(Console.ReadLine());
+                Console.Write("Name: ");
+                string name2 = Convert.ToString(Console.ReadLine());
+
+
+                Person1[i] = new Person(name,name1,name2);
+
+              
+                
+            }
+            Console.WriteLine(Person1[0]);
+            Console.ReadLine();
+        }
+    }
+}
