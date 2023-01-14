4479e1f4ea2587f931d4b319210f75f2b0b1abc3
Bilol Kobilov <115587177+1bilol@users.noreply.github.com>
2023-01-14 14:39:26 +0500

    completed

---
+++ task 6/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_classes6
+{
+    class Rectangle : Shape
+    {
+        public double  side1;
+        public double  side2;
+    }
+}
---
+++ task 6/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_classes6
+{
+    class Shape : Location
+    {
+        public string ToString;
+
+        public double Area;
+
+        public double Perimeter;
+        
+    }
+}
