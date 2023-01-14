c2dfe1a2f82149a8c3e2c672b1b9c6ec1ca38c2f
Bilol Kobilov <115587177+1bilol@users.noreply.github.com>
2023-01-14 15:07:15 +0500

    completed

---
+++ task 7/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_classes7
+{
+    class Car : IVehiculo
+    {
+        private int Fuel;      
+        public Car(int aFuel)
+        {
+            Fuel = aFuel;
+        }
+        public int aFuel
+        {
+            get
+            {
+                return Fuel;
+            }
+            set
+            {
+                Fuel = value;
+            }
+        }
+        public void Drive()
+        {
+            if (Fuel > 0)
+            {
+                Console.WriteLine("Driving");
+            }
+            else
+            {
+                Console.WriteLine("Not fuel");
+            }
+        }
+        public bool Refuel(int amount)
+        {
+            Fuel += amount;
+            return true;
+        }
+    }
+}
---
+++ task 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_classes7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(0);
+            int fuel = Convert.ToInt32(Console.ReadLine());
+            if (car.Refuel(fuel))
+            {
+                car.Drive();
+            }          
+            Console.ReadLine();
+        }
+    }
+}
